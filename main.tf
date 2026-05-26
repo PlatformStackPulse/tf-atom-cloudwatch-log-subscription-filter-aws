bfb3d133dc7b45a7624e4266c8de3f7852173cc4
harry.sarshogh <harry.sarshogh@gmail.com>
2026-05-26 22:48:01 +0100

    feat: implement tf-atom-cloudwatch-log-subscription-filter-aws module

---
+++ main.tf
@@ -0,0 +1,9 @@
+resource "aws_cloudwatch_log_subscription_filter" "this" {
+  count = module.this.enabled ? 1 : 0
+
+  name            = module.this.id
+  log_group_name  = var.log_group_name
+  filter_pattern  = var.filter_pattern
+  destination_arn = var.destination_arn
+  role_arn        = var.role_arn
+}
